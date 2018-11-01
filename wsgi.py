fa77671dba6a9d8f2e2f5d99abe68a81d2db7585
Jean-François Perelgritz <jf.perelgritz@gmail.com>
2018-11-01 23:32:11 +0100

    Type des valeurs d'entrée (datetime)

--- wsgi.py
+++ wsgi.py
@@ -36,12 +36,26 @@ def graphs():
     graph1=graph1_url,
     graph2=graph2_url,
     graph3=graph3_url)
-	
+....
 @application.route('/cfc')
 def horaireCFC():
-	horaire = calcul_horaire(TypeHoraire[0])
-	tableau_url = tableau_de_marche(horaire, horaire['titre'])
-	return render_template('tableau_marche.html', tableau_marche=tableau_url)
+....horaire = calcul_horaire({
+        'titre': 'Samedi (1 train)',
+        'nTrains': 1,
+        'temps_entre_gares': datetime.timedelta(minutes=8),
+        'terminus_gennevilliers': 'Gennevilliers',
+        'terminus_epinay': 'Les Mariniers',
+        'temps_arret_terminus': datetime.timedelta(minutes=0),
+        'depart_limite_premier_train_depot': datetime.datetime(1970, 1, 1, 14, 30),
+        'temps_depot_ferme': datetime.timedelta(minutes=25),
+        'depart_T1_ferme': datetime.datetime(1970, 1, 1, 15, 00),
+        'temps_entre_trains': datetime.timedelta(minutes=32),
+        'heure_limite_arrivee_ferme': datetime.datetime(1970, 1, 1, 19, 10),
+        'nNavettes': 0,
+        'terminus_navettes': 'La Ferme'
+    })
+....tableau_url = tableau_de_marche(horaire, horaire['titre'])
+....return render_template('tableau_marche.html', tableau_marche=tableau_url)
  
 if __name__ == '__main__':
     application.debug = True
