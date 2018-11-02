cffe736de286d5579e526dc62c8d317247010f4d
Jean-François Perelgritz <jf.perelgritz@gmail.com>
2018-11-02 11:21:04 +0100

    Ménage + @application.route("/") ('Hello JF!')

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,3 @@
-# from flask import Flask
-# application = Flask(__name__)
-
-# @application.route("/")
-# def hello():
-    # return "Hello JF!"
-
-# if __name__ == "__main__":
-    # application.run()
-
-
-	
 from flask import Flask, render_template, request
 from graph import build_graph
 from definitions import *
@@ -18,6 +6,10 @@ from marche import tableau_de_marche
  
 application = Flask(__name__)
  
+@application.route("/")
+def hello():
+    return "Hello JF!"
+
 @application.route('/graphs')
 def graphs():
     #These coordinates could be stored in DB
@@ -43,22 +35,6 @@ def horaireCFC():
 		type_horaire = int(request.args.get('type'))
 	except:
 		type_horaire = 0
-	
-	# horaire = calcul_horaire({
-        # 'titre': 'Samedi (1 train)',
-        # 'nTrains': 1,
-        # 'temps_entre_gares': datetime.timedelta(minutes=8),
-        # 'terminus_gennevilliers': 'Gennevilliers',
-        # 'terminus_epinay': 'Les Mariniers',
-        # 'temps_arret_terminus': datetime.timedelta(minutes=0),
-        # 'depart_limite_premier_train_depot': datetime.datetime(1970, 1, 1, 14, 30),
-        # 'temps_depot_ferme': datetime.timedelta(minutes=25),
-        # 'depart_T1_ferme': datetime.datetime(1970, 1, 1, 15, 00),
-        # 'temps_entre_trains': datetime.timedelta(minutes=32),
-        # 'heure_limite_arrivee_ferme': datetime.datetime(1970, 1, 1, 19, 10),
-        # 'nNavettes': 0,
-        # 'terminus_navettes': 'La Ferme'
-	# })
 
 	horaire = calcul_horaire(decodage_type_horaire(TypeHoraire[type_horaire]))
 	tableau_url, pdf_url = tableau_de_marche(horaire, TypeHoraire[type_horaire]['titre'])
