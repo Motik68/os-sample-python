de5c4f549e724ae086c6cb10c529f31192bd4460
Jean-François Perelgritz <jf.perelgritz@gmail.com>
2018-11-02 23:06:06 +0100

    Traitement des paramètres passés en args GET

--- wsgi.py
+++ wsgi.py
@@ -31,13 +31,32 @@ def graphs():
 
 @application.route('/cfc')
 def horaireCFC():
-	type_horaire = request.args.get('type', 0, type=int)
-	
+	type_horaire = request.args.get('type', type=int)
 	liste_types = [type['titre'] for type in TypeHoraire]
+	parametres = {}
+	
+	if type_horaire == None:
+		type_horaire = 0
+		parametres['titre'] = request.args.get('titre', "")
+		parametres['nTrains'] = request.args.get('nTrains', type=int)
+		parametres['temps_entre_gares'] = request.args.get('temps_entre_gares', type=int)
+		parametres['terminus_gennevilliers'] = request.args.get('terminus_gennevilliers')
+		parametres['terminus_epinay'] = request.args.get('terminus_epinay')
+		parametres['temps_arret_terminus'] = request.args.get('temps_arret_terminus', type=int)
+		parametres['depart_limite_premier_train_depot'] = request.args.get('depart_limite_premier_train_depot')
+		parametres['temps_depot_ferme'] = request.args.get('temps_depot_ferme', type=int)
+		parametres['depart_T1_ferme'] = request.args.get('depart_T1_ferme')
+		parametres['temps_entre_trains'] = request.args.get('temps_entre_trains', type=int)
+		parametres['heure_limite_arrivee_ferme'] = request.args.get('heure_limite_arrivee_ferme')
+		parametres['nNavettes'] = request.args.get('nNavettes', type=int)
+		parametres['terminus_navettes'] = request.args.get('terminus_navettes')
+	else:
+		parametres = TypeHoraire[type_horaire].copy()
 
-	horaire = calcul_horaire(decodage_type_horaire(TypeHoraire[type_horaire]))
-	tableau_url, pdf_url = tableau_de_marche(horaire, TypeHoraire[type_horaire]['titre'])
-	return render_template('tableau_marche.html', tableau_marche=tableau_url, tableau_marche_pdf=pdf_url, types=liste_types, choix_type=type_horaire)
+	horaire = calcul_horaire(decodage_type_horaire(parametres))
+	tableau_url, pdf_url = tableau_de_marche(horaire, parametres['titre'])
+	return render_template('tableau_marche.html', tableau_marche=tableau_url, tableau_marche_pdf=pdf_url, 
+							types=liste_types, choix_type=type_horaire, parametres=parametres)
  
 if __name__ == '__main__':
     application.debug = True
